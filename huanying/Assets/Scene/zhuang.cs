70c317c9893a305d5ea25b1f5bf2377aef543a01
zyhmhm <1145247833@qq.com>
2018-11-19 13:05:08 +0800

    项目

---
+++ huanying/Assets/Scene/zhuang.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class zhuang : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+     public void OnMouseUp()
+    {
+        Invoke("OnClick", 0.5F);
+    }
+    void OnClick()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
