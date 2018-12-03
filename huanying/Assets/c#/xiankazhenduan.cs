a79c44f011826d4906e79f97c56e8e83387d0ea4
zyhmhm <1145247833@qq.com>
2018-12-03 17:31:55 +0800

    故障一

---
+++ huanying/Assets/c#/xiankazhenduan.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class xiankazhenduan : MonoBehaviour
+{
+
+    private GameObject gameObject;
+    // Use this for initialization
+    void Start()
+    {
+        gameObject = GameObject.Find("xianshiping/xianshiping/Cube/Quad");
+        Invoke("OnClick", 2.0F);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void OnClick()
+    {
+        gameObject.GetComponent<MeshRenderer>().material.color = Color.black;
+    }
+}
