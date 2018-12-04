126fe91d4b2c0d9317cad750de109907a7beeb36
zyhmhm <1145247833@qq.com>
2018-12-04 15:19:30 +0800

    故障一

--- huanying/Assets/c#/xiankazhenduan.cs
+++ huanying/Assets/c#/xiankazhenduan.cs
@@ -6,20 +6,27 @@ public class xiankazhenduan : MonoBehaviour
 {
 
     private GameObject gameObject;
-    // Use this for initialization
+    //public Material material;//更改后的材质球，可拖拽赋值
+    //gameObject.GetComponent<MeshRenderer>().materials[0].CopyPropertiesFromMaterial(material);//更改材质球
+
+
+    private Texture2D myTexture;
+
     void Start()
     {
         gameObject = GameObject.Find("xianshiping/xianshiping/Cube/Quad");
+        myTexture = (Texture2D)Resources.Load("pic/huaping");
         Invoke("OnClick", 2.0F);
     }
 
-    // Update is called once per frame
     void Update()
     {
 
     }
     void OnClick()
     {
-        gameObject.GetComponent<MeshRenderer>().material.color = Color.black;
+        //gameObject.GetComponent<MeshRenderer>().material.color = Color.black;
+        //gameObject.tag = "pic/huaping.jpg";
+        gameObject.GetComponent<MeshRenderer>().material.mainTexture = myTexture;
     }
 }
